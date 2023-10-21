6a16d63dbaf5fd84ae4f004e4c6f66a20f9614e6
Daniel R <daniel.rojas@utec.edu.pe>
2023-10-21 06:46:20 -0500

    add predict multistep function

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 import pdb
 
-
 app = Flask(__name__)
 
 model = load_model("model/LSTM-ST-Model.keras")
 
-def predict_with_model(time_series_data: NDArray, output_len: int = 1):
+def predict_with_model(time_series_data: NDArray):
     """
     Given a numpy array return the output
     Input: [X_1, X_2, X_3, ..., X_20]
@@ -27,6 +26,22 @@ def predict_with_model(time_series_data: NDArray, output_len: int = 1):
     return denormalized_data
 
 
+def predict_multistep(time_series_data: NDArray, output_len: int = 1):
+    assert output_len >= 1, "Bad value of output len"
+
+    result = []
+
+    input_data = time_series_data
+
+    for _ in range(output_len):
+        [output_val] = predict_with_model(time_series_data=input_data)
+        result.append(output_val)
+
+        # the predicted value is used for the next prediction
+        input_data = input_data[1:]
+        input_data = np.append(input_data, output_val)
+
+
 @app.route("/predict", methods=["POST"])
 def predict():
     """
@@ -38,8 +53,8 @@ def predict():
     if time_series_data is None:
         return "Error"
 
-    output_len = 1
-    predicted_values = predict_with_model(np.array(time_series_data), output_len=output_len)
+    output_len = content.get("output_len", 1)
+    predicted_values = predict_multistep(np.array(time_series_data), output_len=output_len)
     #pdb.set_trace()
     response = {
         "output_len": output_len,
